a5f906faa7500d828ced436741837fc50b0fd33a
Benjamineboson <54313829+Benjamineboson@users.noreply.github.com>
2019-09-08 21:04:22 +0200

    Update SecretWordGenerator.java

--- src/main/java/SecretWordGenerator.java
+++ src/main/java/SecretWordGenerator.java
@@ -1,12 +1,37 @@
 package model;
 
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.Random;
+import java.util.Scanner;
 
 public class SecretWordGenerator {
-    public static String chooseSecretWord(){
+
+    //Jag inser att min path inte kommer vara din path om du kör programmet från din dator. Har ej hittat ett sätt runt det ännu.
+
+    private static File file = new File ("C:\\Users\\Benjamin\\IdeaProjects\\veckaTreGissningsSpelOop\\src\\main\\resources\\Words.txt");
+
+    public static String chooseSecretWord() {
         Random random = new Random();
-        String [] wordList = {"Jazz","Cough","Evil","Paris","Programming","Flower","Apple","Table","Display","Bear"};
+        String[] wordList = wordlistToArray(file);
         String secretWord = wordList[random.nextInt(wordList.length)].toUpperCase();
         return secretWord;
     }
-}
+
+    public static String[] wordlistToArray(File words){
+    String [] wordListArray = new String[213];
+    try {
+        Scanner scan = new Scanner(words);
+        for (int i = 0; i < wordListArray.length ; i++) {
+            wordListArray[i] = scan.next();
+        }
+        return wordListArray;
+    }catch (FileNotFoundException e) {
+        System.out.println("File not found");
+    }
+     return null;
+        }
+
+   }
+
+
